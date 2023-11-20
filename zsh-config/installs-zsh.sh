24b1ba8c6373ccac6ad5a27f201956db727150bb
M O Y <moy@manjaro>
2023-11-20 15:43:25 -0600

    add dotnet Installation Files

--- zsh-config/installs-zsh.sh
+++ zsh-config/installs-zsh.sh
@@ -6,5 +6,6 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 # zsh-syntax-highlighting -> plugins = zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-plugins = zsh-syntax-highlighting
+
+
 
