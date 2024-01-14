12c548a01039d002d451d0ee39417378a18cfe21
M O Y <moy@manjaro.asus>
2024-01-14 17:34:03 -0600

    Changes to be committed: modified:   settings.sh

--- distros/settings.sh
+++ distros/settings.sh
@@ -1,1 +1,5 @@
+# Time and date for the dual boot
 timedatectl set-local-rtc 1 --adjust-system-clock
+
+# Alias
+echo alias c="clear;echo '==============================';ls;echo '=============================='"
