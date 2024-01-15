fbe5bcb5cfc4a245e8e451f543f5b094b7e87e2e
M O Y <moy@manjaro.asus>
2024-01-14 18:22:24 -0600

    modified:   README.md deleted:    distros/script-github.sh modified:   distros/settings.sh

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Linux Installs
 
-Primero que nada ejecutar el los scripts de zsh
+1.Ejecutar el los scripts de zsh
 - zsh
 - plugins
 
+2.Ejecutar el script de installs dependiendo la distribucion ubicado en la carpeta distros
+
+3.Ejecutar el script settings de la misma carpeta y cuando salgan los alias, pegarlos en el archivo de configuracion pero completandolos (ver codigo de script y el resultado que arrojo al ejecutarlo)
--- distros/settings.sh
+++ distros/settings.sh
@@ -4,3 +4,35 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 # Alias
 echo alias c="clear;echo '==============================';ls;echo '=============================='"
 
+echo alias s='
+echo "================================
+"
+echo "Iniciando servicio"
+echo "--------------------------------
+"
+eval "$(ssh-agent -s)"
+echo "
+================================
+"
+echo "Elimiando claves"
+echo "--------------------------------
+"
+ssh-add -D
+echo "
+================================
+"
+echo "Agregando clave"
+echo "--------------------------------
+"
+ssh-add ~/.ssh/gh
+echo "
+================================
+"
+echo "Comprobando conexion"
+echo "--------------------------------
+"
+ssh git@github.com
+echo "
+================================
+"
+'
