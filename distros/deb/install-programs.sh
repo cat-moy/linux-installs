54b8db58387313afd45a0d04d8fcbf0435cac6f0
M O Y <moy@linux>
2024-03-16 21:16:35 -0600

    modified:   install-programs.sh

--- distros/deb/install-programs.sh
+++ distros/deb/install-programs.sh
@@ -1,3 +1,30 @@
 # Update
 sudo apt update -y
 sudo apt upgrade -y
+
+# Snap
+sudo apt install snapcraft -y
+
+# Xclip
+sudo apt install xclip -y
+
+# Python
+sudo apt install python3
+
+# Git
+sudo apt install git -y
+
+# Tmux
+sudo apt install tmux -y
+
+# Neovim
+sudo apt install neovim -y
+
+# Code
+sudo snap install code --classic
+
+# Pycharm
+sudo snap install pycharm-community --classic
+
+
+
